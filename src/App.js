48fcc3342bb764b3654cbcae7029da9ab16409f0
HernanMolinaDev <hernanjmolinadev@gmail.com>
2023-10-09 15:38:57 -0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
-import './App.css';
-import './components/PerfilCard'
+import '../src/css/style.css'
 
 function App() {
 
   return (
-        <div>
+
+          <div>
           <NavBar/>
           <Home/>
           </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,34 @@
-import react from 'react'
+
 import CarWidget from './CarWidget'
+import logo from '../img/pngegg.png'
 const NavBar=({})=>{
     return(
-            <nav className="navbar navbar-inverse">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <a className="navbar-brand" href="">WebSiteName</a>
-                    </div>
-                    <ul className="nav navbar-nav">
-                    <li className="active"><a href="">Home</a></li>
-                    <li className="active"><a href="">Productos</a></li>
-                    <li className="active"><a href="">Nosotros</a></li>
-                    <li><a href="#"><CarWidget/></a></li>
-                    </ul>
-                    <form className="navbar-form navbar-left" action="/action_page.php">
-                    <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Search" name="search"/>
-                    </div>
-                    <button type="submit" className="btn btn-default">Submit</button>
-                    </form>
-                </div>
-            </nav>
+        <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand" href="index.html">
+            <img src={logo} width="30" height="30" alt="" className="img-logo-nav"/>
+        </a>
+        <a className="navbar-brand nav-link" href="../public/index.html">DHARMA</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>           
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+                <a className="nav-link nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link nav-link" href="">Productos</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link nav-link" href="">Contactanos</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link nav-link" href=""><CarWidget/></a>
+            </li>
+            </ul>
+        </div>
+        </nav>
+
     )
 }
 export default NavBar
